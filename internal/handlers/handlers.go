b3e30bddd939d5b845dd5c17c1c87338fc52c94b
anaumcik <naumcik.inno.co@gmail.com>
2025-09-25 16:42:25 +0300

    add status ok and content type

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -21,6 +21,9 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
 	cwd, err := os.Getwd()
 	if err != nil {
 		log.Printf("Ошибка при получении текущей директории: %v", err)
